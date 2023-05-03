1fc0175677ee8a5077d58dffaa2af33736aa3e86
Lucas <lucas.txsan@gmail.com>
2023-05-03 19:32:38 -0300

    Codigo revisado

--- src/components/SecaoIndex1/SecaoIndex1.jsx
+++ src/components/SecaoIndex1/SecaoIndex1.jsx
@@ -1,33 +1,37 @@
 import "./SecaoIndex1.css";
 
-
 export function SecaoIndex1() {
   return (
     <section>
-        <div className="eclipse"></div>
+      <div className="eclipse"></div>
+      {/* por enquanto vamos deixar esse eclipse aqui, até encontrarmos uma maneira mais eficiente de reproduzir */}
 
-            <img src="./assets/Caminhao1.png" alt="" />
+      <img src="./assets/Caminhao1.png" alt="" />
+      {/* a imagem do caminhão também é provisória, até acharmos uma melhor */}
+      <h1>
+        <span>Movimente</span> sua vida com tranquilidade. <br /> Deixe o
+        <span> transporte</span> por nossa conta.
+      </h1>
 
-        <h1><span className="tituloMovimente">Movimente</span> sua vida com tranquilidade. <br/> Deixe o <span  className="tituloTransporte">transporte</span> por nossa conta.</h1>
-            
-            <p>Se você está procurando um serviço de fretes ou carretos<br/>confiável e eficiente, então você veio ao lugar certo! Não <br/>deixe para depois peça “<span>Now</span>” com a <span>MoveNow</span>.</p>
+      <p>
+        Se você está procurando um serviço de fretes ou carretos
+        <br />
+        confiável e eficiente, então você veio ao lugar certo! Não <br />
+        deixe para depois, peça “<span>Now</span>” com a <span>MoveNow</span>.
+      </p>
 
-        <div className="buttons">
-            <a className="buttonIndex1">Solicite um carreto</a>
-            <a className="buttonIndex2">Seja um motorista</a>
-        </div>
+      <div className="wrapperButtons">
+        <button>Solicite um carreto</button>
+        <button>Seja um motorista</button>
+      </div>
 
-        <div className="parcerias">
-            <img src="./assets/logoWaze.png" className="imagemParceira"/>
-            <img src="./assets/logoShell.png" className="imagemParceira"/>
-            <img src="./assets/logoPetrobras.png" className="imagemParceira"/>
-            <img src="./assets/logoIpiranga.png" className="imagemParceira"/>
-            <img src="./assets/logoRutter.png" className="imagemParceira"/>
-        </div>
-  
+      <div className="parcerias">
+        <img src="./assets/logoWaze.png" className="imagemParceira" />
+        <img src="./assets/logoShell.png" className="imagemParceira" />
+        <img src="./assets/logoPetrobras.png" className="imagemParceira" />
+        <img src="./assets/logoIpiranga.png" className="imagemParceira" />
+        <img src="./assets/logoRutter.png" className="imagemParceira" />
+      </div>
     </section>
   );
 }
-
-
-

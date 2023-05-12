1fa6b975b78efc6159d5c66c36cb61fabdcd92c4
Lucas <lucas.txsan@gmail.com>
2023-05-12 16:55:48 -0300

    Alterações pontuais

--- src/components/SecaoIndex1/SecaoIndex1.jsx
+++ src/components/SecaoIndex1/SecaoIndex1.jsx
@@ -3,7 +3,6 @@ import { Partnerships } from "../Partnerships/Partnerships";
 import "./SecaoIndex1.css";
 
 export function SecaoIndex1() {
-
   const styleButtonPrimary = {
     color: "#000000",
     background: "var(--orange)",
@@ -17,38 +16,32 @@ export function SecaoIndex1() {
   };
 
   return (
-    <div className="sectionOne">
-      <section>
-        {/* <div className="eclipse"></div> */}
-        {/* por enquanto vamos deixar esse eclipse aqui, até encontrarmos uma maneira mais eficiente de reproduzir */}
+    <section>
+      {/* <div className="eclipse"></div> */}
+      {/* por enquanto vamos deixar esse eclipse aqui, até encontrarmos uma maneira mais eficiente de reproduzir */}
 
-        {/* a imagem do caminhão também é provisória, até acharmos uma melhor */}
+      {/* a imagem do caminhão também é provisória, até acharmos uma melhor */}
 
-        <div className="wrapperContent">
-          <h1>
-            <span>Movimente</span> sua vida com tranquilidade. <br /> Deixe o
-            <span> transporte</span> por nossa conta.
-          </h1>
+      <div className="wrapperContent">
+        <h1>
+          <span>Movimente</span> sua vida com tranquilidade. <br /> Deixe o
+          <span> transporte</span> por nossa conta.
+        </h1>
 
-          <p>
-            Se você está procurando um serviço de fretes ou carretos
-            <br />
-            confiável e eficiente, então você veio ao lugar certo! Não <br />
-            deixe para depois, peça “<span>Now</span>” com a <span>MoveNow</span>.
-          </p>
+        <p>
+          Se você está procurando um serviço de fretes ou carretos
+          <br />
+          confiável e eficiente, então você veio ao lugar certo! Não <br />
+          deixe para depois, peça “<span>Now</span>” com a <span>MoveNow</span>.
+        </p>
 
-          <div className="wrapperButtons">
-            <Button style={styleButtonPrimary} name="Solicite um carreto" />
-            <Button style={styleButtonSecondary} name="Seja um motorista" />
-          </div>
+        <div className="wrapperButtons">
+          <Button style={styleButtonPrimary} name="Solicite um carreto" />
+          <Button style={styleButtonSecondary} name="Seja um motorista" />
         </div>
+      </div>
 
-        <img src="../src/assets/Caminhao1.png" alt="" />
-
-      </section>
-
-      <Partnerships />
-      
-    </div>
+      <img src="../src/assets/Caminhao.png" alt="" />
+    </section>
   );
 }
--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -1,11 +1,13 @@
 import { Header } from "../../components/Header/Header";
 import { SecaoIndex1 } from "../../components/SecaoIndex1/SecaoIndex1";
+import { Partnerships } from "../../components/Partnerships/Partnerships";
 
 export function LandingPage() {
   return (
     <>
       <Header />
       <SecaoIndex1 />
+      <Partnerships />
     </>
   );
 }

cccd628da2a0fa70902cbfab40301e7bd9b31a32
Lucas <lucas.txsan@gmail.com>
2023-05-13 17:41:59 -0300

    Responsividade (Parcerias e Section1) arrumadas

--- src/components/Partnerships/Partnerships.jsx
+++ src/components/Partnerships/Partnerships.jsx
@@ -1,27 +1,23 @@
 import "./Partnerships.css";
 
 export function Partnerships() {
-
   return (
-
     <div className="parcerias">
-      
-        <div className="parceiro" id="parcerio1">
-            <img src="../src/assets/image 1.png" className="imagemParceira" />
-        </div>
-        <div className="parceiro" id="parcerio2">
-            <img src="../src/assets/image 2.png" className="imagemParceira" />
-        </div>
-        <div className="parceiro" id="parcerio3">
-            <img src="../src/assets/g32.svg" className="imagemParceira" />
-        </div>
-        <div className="parceiro" id="parcerio4">
-            <img src="../src/assets/image 3.png" className="imagemParceira" />
-        </div>
-        <div className="parceiro" id="parcerio5">
-            <img src="../src/assets/logoRutter.png" className="imagemParceira" />
-        </div>
-        
+      <div className="parceiro">
+        <img src="../src/assets/image 1.png" />
+      </div>
+      <div className="parceiro">
+        <img src="../src/assets/image 2.png" />
+      </div>
+      <div className="parceiro">
+        <img src="../src/assets/g32.svg" />
+      </div>
+      <div className="parceiro">
+        <img src="../src/assets/image 3.png" />
+      </div>
+      <div className="parceiro">
+        <img src="../src/assets/logoRutter.png" />
+      </div>
     </div>
   );
 }
